a3fddfe09b9f594208182e83952da7a085ca46ee
Shawn Dempsey <shwanton@gmail.com>
2025-03-17 17:09:11 -0700

    Add native module getter/setter for setting the Keychain Alias

--- src/NativeRNCWebViewModule.ts
+++ src/NativeRNCWebViewModule.ts
@@ -3,11 +3,15 @@ import { TurboModuleRegistry } from 'react-native';
 import { Double } from 'react-native/Libraries/Types/CodegenTypes';
 
 export interface Spec extends TurboModule {
-  isFileUploadSupported(): Promise<boolean>;
   shouldStartLoadWithLockIdentifier(
     shouldStart: boolean,
     lockIdentifier: Double
   ): void;
+  // Android only
+  isFileUploadSupported(): Promise<boolean>;
+  getCustomCertificateKeychainAlias(): string | null;
+  setCustomCertificateKeychainAlias(alias: string | null): void
+  // !Android only
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('RNCWebViewModule');
